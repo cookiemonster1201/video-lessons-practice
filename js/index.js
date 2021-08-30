3ec8be447ace586c9e9ff3c0514b18b91ee47f4e
Alona Mykytiuk <m.j.aliona@gmail.com>
2021-08-30 13:26:05 +0300

    chart done

--- js/index.js
+++ js/index.js
@@ -1,48 +1,161 @@
 //юзать без бандлера
 
+const GLOBAL_MEAN_TEMPERATURE = 14;
+
 function fetchData() {
-  return fetch("./ZonAnn.Ts+dSST.csv")
-    .then((response) => response.text())
-    .then((data) => Papa.parse(data, { header: true }));
+  return fetch("../ZonAnn.Ts+dSST.csv").then((response) => response.text());
 }
 
-const context = document.getElementById("js-chart").getContext("2d");
+function parseData(data) {
+  return Papa.parse(data, { header: true }).data;
+}
 
-console.log(context);
+function getMetaData(dataArr) {
+  return dataArr.reduce(
+    (acc, entry) => {
+      acc.years.push(entry.Year);
+      acc.globeTemps.push(+entry.Glob + GLOBAL_MEAN_TEMPERATURE);
+      acc.nHemTemps.push(+entry.NHem + GLOBAL_MEAN_TEMPERATURE);
+      acc.sHemTemps.push(+entry.SHem + GLOBAL_MEAN_TEMPERATURE);
+      return acc;
+    },
+    { years: [], globeTemps: [], nHemTemps: [], sHemTemps: [] }
+  );
+}
 
-const myChart = new Chart(context, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+function drawChart(labels, global, north, south) {
+  const context = document.getElementById("js-chart").getContext("2d");
+
+  const temperatureChart = new Chart(context, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Globe",
+          data: global,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          fill: false,
+        },
+        {
+          label: "NHem",
+          data: north,
+          backgroundColor: "green",
+          borderColor: "green",
+          borderWidth: 1,
+          fill: false,
+        },
+        {
+          label: "SHem",
+          data: south,
+          backgroundColor: "blue",
+          borderColor: "blue",
+          borderWidth: 1,
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          ticks: {
+            callback: function (value, index, values) {
+              return value + "C";
+            },
+          },
+        },
       },
     },
-  },
-});
+  });
+}
+
+fetchData()
+  .then(parseData)
+  .then(getMetaData)
+  .then(({ years, globeTemps, nHemTemps, sHemTemps }) => {
+    drawChart(years, globeTemps, nHemTemps, sHemTemps);
+  });
+
+// function fetchData() {
+//   fetch("../ZonAnn.Ts+dSST.csv")
+//     .then((response) => response.text())
+//     .then((data) => {
+//       const parseData = Papa.parse(data, { header: true }).data;
+
+//       const metaData = parseData.reduce(
+//         (acc, entry) => {
+//           acc.years.push(entry.Year);
+//           acc.globeTemps.push(+entry.Glob + GLOBAL_MEAN_TEMPERATURE);
+//           acc.nHemTemps.push(+entry.NHem + GLOBAL_MEAN_TEMPERATURE);
+//           acc.sHemTemps.push(+entry.SHem + GLOBAL_MEAN_TEMPERATURE);
+//           return acc;
+//         },
+//         { years: [], globeTemps: [], nHemTemps: [], sHemTemps: [] }
+//       );
+
+//longer way-------------
+//   const years = parseData.map((entry) => entry.Year);
+
+//   const globeTemps = parseData.map(
+//     (entry) => +entry.Glob + GLOBAL_MEAN_TEMPERATURE
+//   );
+
+//   const nHemTemps = parseData.map(
+//     (entry) => +entry.NHem + GLOBAL_MEAN_TEMPERATURE
+//   );
+
+//   const sHemTemps = parseData.map(
+//     (entry) => +entry.SHem + GLOBAL_MEAN_TEMPERATURE
+//   );
+//longer way-------------
+
+//       const context = document.getElementById("js-chart").getContext("2d");
+
+//       const myChart = new Chart(context, {
+//         type: "line",
+//         data: {
+//           labels: metaData.years,
+//           datasets: [
+//             {
+//               label: "Globe",
+//               data: metaData.globeTemps,
+//               backgroundColor: "rgba(255, 99, 132, 0.2)",
+//               borderColor: "rgba(255, 99, 132, 1)",
+//               borderWidth: 1,
+//               fill: false,
+//             },
+//             {
+//               label: "NHem",
+//               data: metaData.nHemTemps,
+//               backgroundColor: "green",
+//               borderColor: "green",
+//               borderWidth: 1,
+//               fill: false,
+//             },
+//             {
+//               label: "SHem",
+//               data: metaData.sHemTemps,
+//               backgroundColor: "blue",
+//               borderColor: "blue",
+//               borderWidth: 1,
+//               fill: false,
+//             },
+//           ],
+//         },
+//         options: {
+//           scales: {
+//             y: {
+//               ticks: {
+//                 // Include a dollar sign in the ticks
+//                 callback: function (value, index, values) {
+//                   return value + "C";
+//                 },
+//               },
+//             },
+//           },
+//         },
+//       });
+//     });
+// }
